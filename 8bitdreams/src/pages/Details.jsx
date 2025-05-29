01d0c0bb8dbecb7331c69b93d294bc80b8964306
tismajo <mariajosegironisidro@gmail.com>
2025-05-29 16:54:08 -0600

    likes system added

--- 8bitdreams/src/pages/Details.jsx
+++ 8bitdreams/src/pages/Details.jsx
@@ -1,20 +1,30 @@
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { CartContext } from '../context/CartContext';
+import { useContext, useState } from 'react';
 
 import products from '../data/products';
 import '../styles/Details.css';
-import { useContext } from 'react';
 
 export default function Details() {
   const { id } = useParams();
   const { addToCart } = useContext(CartContext);
   const product = products.find((product) => product.id === parseInt(id));
-  if (!product) return <p>No se pudo encontrar el producto.</p>;
-
+  
   const discountedPrice = product.discount
     ? product.price - (product.price * product.discount) / 100
     : product.price;
+  const [likes, setLikes] = useState(() => {
+    const storedLikes = localStorage.getItem(`likes-${product.id}`);
+    return storedLikes ? parseInt(storedLikes, 10) : product.likes || 0;
+  });
+
+    const handleLike = () => {
+    const newLikes = likes + 1;
+    setLikes(newLikes);
+    localStorage.setItem(`likes-${product.id}`, newLikes);
+  };
+
+  if (!product) return <p>No se pudo encontrar el producto.</p>;
 
   return (
     <div className="productDetails-container">
@@ -24,6 +34,10 @@ export default function Details() {
         <button onClick={() => addToCart(product)} className="add-button">
           Añadir al carrito
         </button>
+
+        <div className="like-container">
+          <button onClick={handleLike} className="like-button">❤️ {likes} Likes</button>
+        </div>
       </div>
 
       <div className="productDetails-right">
